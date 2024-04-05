1af9c4db51e42b6d8f48efd5a489507ef293c024
SAID BIO WEDE <saidbiowede@gmail.com>
2024-04-05 13:47:26 +0100

    Mobile done

--- pages/notes.vue
+++ pages/notes.vue
@@ -3,6 +3,7 @@
 
     const api_token = ref(null)
     const notes = ref([])
+    const displayedNote = ref([])
     const newTitle = ref("")
     const newNote = ref("")
     const newModal = ref(false)
@@ -10,6 +11,7 @@
     const editIndex = ref(0)
     const editTitle = ref("")
     const editNote = ref("")
+    const search = ref("")
     const debounceTimer = ref(null)
 
     async function refreshNotes(){
@@ -20,6 +22,11 @@
                 },
             }).then((response) => {
                 notes.value = response.data
+                displayedNote.value = []
+                notes.value.forEach(element => {
+                    element.visible = true
+                    displayedNote.value.push(element)
+                });
                 //console.log(response)
             }).catch((error) => {
                 navigateTo('/')
@@ -98,10 +105,18 @@
             }
         }, 500)
     }
+    function handleSearch(){
+        const subString = search.value.toLocaleLowerCase()
+        displayedNote.value.forEach(element => {
+            if(!element.title.toLocaleLowerCase().includes(subString)){
+                element.visible = false
+            }
+        });
+    }
     if(typeof localStorage != "undefined"){
         api_token.value = localStorage.getItem("api_token") || null
         if(!api_token.value){
-            navigateTo('/notes')
+            navigateTo('/')
         }
     }
     refreshNotes()
@@ -110,16 +125,16 @@
 </script>
 <template>
     <div class="bg-[#f1f5f9] pt-10 min-h-screen flex justify-center">
-        <div class="w-[800px]">
+        <div class="md:w-[800px] w-[390px]">
             <div class="flex justify-between gap-5">
                 <div class="flex-grow relative">
                     <i class="pi pi-search absolute top-2/4 -mt-2 left-3 text-neutral-400" />
-                    <InputText icon="pi pi-plus" placeholder="Search notes" type="text" class="rounded-full border text-sm pl-10 border-neutral-300 h-10 w-full"/>
+                    <InputText icon="pi pi-plus" v-model="search" @input="handleSearch" placeholder="Search notes" type="text" class="rounded-full outline-none border text-sm pl-10 border-neutral-300 h-10 w-full"/>
                 </div>
                 <Button @click="showNewModal" class="text-white flex-shrink-0 bg-[#4f46e5] h-10 px-4 text-sm rounded-full" icon="pi pi-plus" label="New note"></Button>
             </div>
-            <div class="flex flex-col items-stretch gap-4 max-h-screen flex-wrap py-4 text-xs">
-                <Card v-for="(note, index) in notes" class="w-60" @click="openEdit(note.id)">
+            <div class="flex md:flex-col items-start gap-4 max-h-screen flex-wrap py-4 pb-40 overflow-scroll text-xs">
+                <Card v-for="(note, index) in notes" :class="`md:w-60 w-full ${(!note.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()) && !note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()) )? 'hidden' : ''}`" @click="openEdit(note.id)">
                     <template #content>
                         <h2 class=" font-bold mb-3">{{ note.title }}</h2>
                         <p class=" text-wrap overflow-hidden">{{ note.content }}</p>
